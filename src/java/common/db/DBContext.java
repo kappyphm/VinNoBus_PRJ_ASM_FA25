49b76bf24613feb7023ef6755eb531ebceb534ed
Phạm Gia Khánh <me.khanhphm@gmail.com>
2025-09-24 20:42:26 +0700

    Create DBContext.java

---
+++ src/java/common/db/DBContext.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common.db;
+
+import java.sql.*;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kappyphm
+ */
+public class DBContext {
+
+    private final String user = "sa";
+    private final String pass = "sa";
+    private final String url = "jdbc:sqlserver://localhost:1433;databaseName=AuthenDB";
+
+    public DBContext() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Không tìm thấy JDBC Driver!", e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, pass);
+    }
+
+    public static void main(String[] args) {
+        try {
+            DBContext db = new DBContext();
+            String sql = "INSERT INTO Users (username, email, hashedPass) OUTPUT inserted.uid VALUES(?,?,?)";
+            Connection cnn = db.getConnection();
+            try (PreparedStatement stm = cnn.prepareStatement(sql)) {
+                stm.setString(1, "admin");
+                stm.setString(2, "admin");
+                stm.setString(3, "admin");
+                
+                ResultSet rs = stm.executeQuery();
+                
+                UUID newId = null;
+                if (rs.next()) {
+                    newId = UUID.fromString(rs.getString("uid"));
+                    System.out.println(newId);
+                }
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
